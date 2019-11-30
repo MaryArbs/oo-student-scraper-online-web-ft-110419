8c20739145d1d06e2e0a3ecb596e453b5918ac20
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-30 00:28:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
         self.new(student_hash)
       end
     end
-  
+
     def add_student_attributes(attributes_hash)
       attributes_hash.each do |k, v|
         self.send(("#{k}="), v)
